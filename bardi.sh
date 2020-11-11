2aa9ad85993e0ea6127ee4e111928f943ba8e8d4
cpfb900 <74002115+cpfb900@users.noreply.github.com>
2020-11-11 20:48:40 +0700

    Add files via upload

---
+++ bardi.sh
@@ -0,0 +1,64 @@
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 2
+echo
+toilet -f big -F gay RIZZ CHANNEL SC
+echo $green"Selamat Masuk , Di SC FB Checkpoint Remover V.0.1"
+echo
+sleep 2
+echo $red"Script v.0.1 adalah sc tahap pengembangan:v"
+echo
+sleep 1
+echo $red"45% Work/65% Tidak Work! Server By: https://loadfbremover.co>
+echo
+echo $yellow"Tool By RIZZ CHANNEL ! Pastikan Koneksi Stabil Silahkan P>
+echo
+echo $white"1.) Tools FB Sesi Biasa"
+echo
+echo $red"INFO:Mau Beli Jasa Bengkel Sesi Saya? Komen Divideo Ini!"
+echo
+echo $white"2.) Tools FB Permanen(TAHAP PENGEMBANGAN TODD)"
+echo
+sleep 2
+echo $red"EXIT"
+echo $white
+read -p "PILIH MANA ANJING : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Rizz Script Rate 8/10
+echo $green"Buka Akun Kesesi Via Server https://loadfbremover.com"
+sleep 1
+echo
+echo $red"LOGIN AKUN FB LU YANG KE CP Pastikan Login Dengan Benar Agar>
+sleep 2
+echo $white"No.HP/Email:"
+read target
+echo $cyan"loading..."
+echo $white"Password:"
+read target
+echo $red"Coba Anda Login"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $red"Server Ini Sedang Perbaikan Stay Tune Di YT Kami!"
+echo
+echo $yellow
+figlet"Auto Exit In 3 Second"
+sleep 3
+exit
+fi
+
+if [ $bro = 0 ] || [$bro = 0 ]
+then
+clear
